7579f29b0ce7aa1ca1a83ac4d7f76e96ea7ae1cf
logical-luke <luke@logical-luke.com>
2024-10-11 15:49:06 +0200

    :zap: Add Vite Plugins and Update Configurations
    Integrated new Vite plugins including vite-tsconfig-paths and rollup-plugin-visualizer for enhanced build processes. Updated tsconfig and package configurations to support these integrations, and added optimization settings to the Vite config file.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import EnvironmentPlugin from 'vite-plugin-environment';
 import { fileURLToPath, URL } from "node:url";
+import { splitVendorChunkPlugin } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     server: {
@@ -15,6 +18,13 @@ export default defineConfig({
             API_BASE_URL: 'https://api.makeitlogical.io/v1',
             NODE_ENV: process.env.NODE_ENV || 'development'
         }),
+        splitVendorChunkPlugin(),
+        visualizer({
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+        }),
+        tsconfigPaths(), // Add this to resolve TypeScript paths
     ],
     resolve: {
         alias: {
@@ -22,11 +32,29 @@ export default defineConfig({
         },
     },
     build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            },
+        },
         rollupOptions: {
             output: {
-                manualChunks: {
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                },
+                chunkFileNames: (chunkInfo) => {
+                    const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : [];
+                    const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]';
+                    return `js/${fileName}/[name].[hash].js`;
                 },
             },
         },
+    },
+    optimizeDeps: {
+        include: ['vue', 'vue-router'], // Add your main dependencies here
     },
 });
