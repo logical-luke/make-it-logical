779f7042ef2c99836839b56baadb1eb5e3814223
logical-luke <luke@logical-luke.com>
2024-10-11 15:56:36 +0200

    Revert ":zap: Add Vite Plugins and Update Configurations"
    This reverts commit 7579f29b0ce7aa1ca1a83ac4d7f76e96ea7ae1cf.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import EnvironmentPlugin from 'vite-plugin-environment';
 import { fileURLToPath, URL } from "node:url";
-import { splitVendorChunkPlugin } from 'vite';
-import { visualizer } from 'rollup-plugin-visualizer';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     server: {
@@ -18,13 +15,6 @@ export default defineConfig({
             API_BASE_URL: 'https://api.makeitlogical.io/v1',
             NODE_ENV: process.env.NODE_ENV || 'development'
         }),
-        splitVendorChunkPlugin(),
-        visualizer({
-            open: true,
-            gzipSize: true,
-            brotliSize: true,
-        }),
-        tsconfigPaths(), // Add this to resolve TypeScript paths
     ],
     resolve: {
         alias: {
@@ -32,29 +22,11 @@ export default defineConfig({
         },
     },
     build: {
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                drop_console: true,
-                drop_debugger: true,
-            },
-        },
         rollupOptions: {
             output: {
-                manualChunks: (id) => {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                },
-                chunkFileNames: (chunkInfo) => {
-                    const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : [];
-                    const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]';
-                    return `js/${fileName}/[name].[hash].js`;
+                manualChunks: {
                 },
             },
         },
-    },
-    optimizeDeps: {
-        include: ['vue', 'vue-router'], // Add your main dependencies here
     },
 });
