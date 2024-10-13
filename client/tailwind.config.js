00f853c0ae90c354dd2532fe57ff453c1135cdde
logical-luke <luke@logical-luke.com>
2024-10-13 21:19:06 +0200

    :bug: Refactor Tailwind config for consistency and future options
    Enclose all configuration strings with double quotes for uniformity. Added the `future` option to handle hover functionality conditional on support. This ensures the Tailwind configuration code is cleaner and more maintainable.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,36 +1,36 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: ['selector'],
+  darkMode: ["selector"],
+  future: {
+    hoverOnlyWhenSupported: true
+  },
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Sinkin Sans"', 'sans-serif'],
-      },
-      future: {
-        hoverOnlyWhenSupported: true,
+        sans: ["\"Sinkin Sans\"", "sans-serif"]
       },
       transitionDuration: {
-        '1300': '2000ms',
-        '1500': '2000ms',
-        '2000': '2000ms',
+        "1300": "2000ms",
+        "1500": "2000ms",
+        "2000": "2000ms"
       },
       colors: {
-        'black': '#05040E',
-        'honolulu-blue': {
-          DEFAULT: '#197bbd',
-          50: '#cbe6f8',
-          100: '#96cdf1',
-          200: '#62b4eb',
-          300: '#2e9be4',
-          400: '#197bbd',
-          500: '#146398',
-          600: '#0f4a72',
-          700: '#0a314c',
-          800: '#051926',
-          900: '#020c13',
-        },
-      },
-    },
-  },
-}
+        "black": "#05040E",
+        "honolulu-blue": {
+          DEFAULT: "#197bbd",
+          50: "#cbe6f8",
+          100: "#96cdf1",
+          200: "#62b4eb",
+          300: "#2e9be4",
+          400: "#197bbd",
+          500: "#146398",
+          600: "#0f4a72",
+          700: "#0a314c",
+          800: "#051926",
+          900: "#020c13"
+        }
+      }
+    }
+  }
+};
