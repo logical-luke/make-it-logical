639f3ee12aeb53cf61528661bfe81f04cceee5f5
logical-luke <luke@logical-luke.com>
2024-10-15 14:16:34 +0200

    :mag: Update ContactSection title and remove redundant paragraph
    Revised the title to "Get Digital Solutions Now" for clarity. Removed an unnecessary paragraph to streamline the content, making the section more concise and focused.

--- client/src/components/ContactSection.vue
+++ client/src/components/ContactSection.vue
@@ -7,16 +7,11 @@ import HomePageSection from "@/components/HomePageSection.vue";
 
 <template>
   <HomePageSection
-    title="Solve Your Problems Now"
+    title="Get Digital Solutions Now"
     link-target="/contact"
     link-text="Let's Talk"
   >
     <div class="flex flex-col gap-4">
-      <p class="md:text-lg mb-8 max-w-4xl">
-        {{ t("Get digital solutions that don't just work – they") }}
-        <span class="font-semibold">{{ t("wow") }}</span
-        >.
-      </p>
       <p class="text-lg md:text-2xl max-w-4xl">
         {{ t("Your vision, our expertise") }}
       </p>
